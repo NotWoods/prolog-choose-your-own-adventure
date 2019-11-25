2c5598b2bd5104e7b71f16c69d1ededdc3ec6111
daphliu <daphne.liu97@gmail.com>
2019-11-24 16:01:51 -0800

    Replaced describe options with numbers

--- game.pl
+++ game.pl
@@ -15,26 +15,29 @@ current_state(start).
 
 3 :- move(3).
 
-
 /* beginning instructions */
+
 start :-
-    write('You are at a prolog conference and have been logical for 7 hours straight.'), nl,
-    write('Taking advantage of all of the free soda and coffee supplied at the event,'), nl,
-    write('you realize that your bladder is quite full.'), nl,
-    write('GOTO (lost_in_school)').
+        write('You are at a prolog conference and have been logical for 7 hours straight.'), nl,
+        write('Taking advantage of all of the free soda and coffee supplied at the event,'), nl,
+        write('you realize that your bladder is quite full.'), 
+        nl.
 
 /* end of game */
 
-// find_bathroom
 success :-
         nl,
-        write('You can’t believe why you didn’t think of asking anyone earlier. You make it to the bathroom and carry on with your hackathon. Quest: completed. Please enter the halt command.'),
+        write('You can’t believe why you didn’t think of asking anyone earlier.'), nl,
+        write('You make it to the bathroom and carry on with your conference.'), nl,
+        write('Quest: completed.').
+        write('Please enter the halt command.'),
         nl.
 
-// peed_pants
 fail :-
         nl,
-        write('You peed your pants. Quest: failed. Quest: failed. Please enter the halt command.'),
+        write('You peed your pants.'), n1,
+        write('Quest: failed.').
+        write('Please enter the halt command.'),
         nl.
 
 /* instructions */
@@ -45,7 +48,7 @@ instructions :-
         write('Available commands are:'), nl,
         write('start.                        -- to start the game.'), nl,
         write('1.  2.  3                    -- to choose the path.'), nl,
-        write('instructions.              -- to see this message again.'), nl,
+        write('instructions.                -- to see this message again.'), nl,
         write('halt.                          -- to end the game and quit.'), nl,
         nl.
 
@@ -53,135 +56,128 @@ instructions :-
 
 describe(lost_in_school) :-
     write('However, you don’t know where the bathroom is.'), nl,
-    write('Do you wander around? (friend)'), nl,
-    write('Do you go pee in a bush? (people_walking_by)'), nl,
-    write('Do you look at the building directory? (janitor_closet)').
+    write('Do you wander around? (1)'), nl,
+    write('Do you go pee in a bush? (2)'), nl,
+    write('Do you look at the building directory? (3)').
 
 describe(friend) :-
     write('You bump into your friend from your haskell class.'), nl,
     write('She needs to tell you something.'), nl,
-    write('Do you listen to her? (drones_on)'), nl,
-    write('Do you avoid conversation? (kicks_bladder)').
+    write('Do you listen to her? (1)'), nl,
+    write('Do you avoid conversation? (2)').
 
 describe(drones_on) :-
     write('You friend begins to ramble on about algorithms and functional programming.'), nl,
     write('You can\'t hold on for much longer.'), nl,
-    write('Do you say you need to go? (need_to_go)'), nl,
-    write('Do you cut her off? (kicks_bladder)').
+    write('Do you say you need to go? (1)'), nl,
+    write('Do you cut her off? (2)').
 
 describe(kicks_bladder) :-
-    write('Your friend throws a fit and kicks you in the bladder.'), nl,
-    write('GOTO (peed_pants)').
+    write('Your friend throws a fit and kicks you in the bladder.').
 
 describe(need_to_go) :-
     write('You really need to go.'), nl,
     write('You begin to hallucinate about waterfalls.'), nl,
-    write('You run home.'), nl,
-    write('GOTO (forgot_key)').
+    write('You run home.').
 
 describe(forgot_key) :-
     write('You reach in your back pocket and find that you forgot your keys.'), nl,
     write('No one is home.'), nl,
-    write('Do you find a public bathroom? (public_bathroom)'), nl,
-    write('Do you call your roomate a wait? (peed_pants)').
+    write('Do you find a public bathroom? (1)'), nl,
+    write('Do you call your roomate a wait? (2)').
 
 describe(public_bathroom) :-
     write('You head to the local coffee store.'), nl,
     write('There is a huge lineup for the bathroom.'), nl,
-    write('Do you wait in line? (wait_in_line)'), nl,
-    write('Do you go pee in a bush? (people_walking_by)').
+    write('Do you wait in line? (1)'), nl,
+    write('Do you go pee in a bush? (2)').
 
 describe(wait_in_line) :-
     write('You decide to wait in line.'), nl,
     write('You are growing impatient.'), nl,
-    write('Do you demand to go first? (demand_pee)'), nl,
-    write('Do you go pee in a bush? (people_walking_by)').
+    write('Do you demand to go first? (1)'), nl,
+    write('Do you go pee in a bush? (2)').
 
 describe(demand_pee) :-
     write('You demand to go first but the person glares at you.'), nl,
-    write('Do you pee your pants in defiance? (peed_pants)').
-
-describe(peed_pants) :-
-    write('You peed your pants.'), nl.
-    write('Quest: failed.').
+    write('Do you pee your pants in defiance? (1)').
 
 describe(people_walking_by) :-
     write('You see people walking by you.'), nl,
-    write('Do you pee anyway? (campus_security)'), nl,
-    write('Do you wait for them to leave? (need_to_go)').
+    write('Do you pee anyway? (1)'), nl,
+    write('Do you wait for them to leave? (2)').
 
 describe(campus_security) :-
     write('Before you can start,'), nl,
     write('campus security spots you and begins to chase after you.'), nl,
-    write('Do you run home? (forgot_key)'), nl,
-    write('Do you run back to school? (lost_in_school)').
+    write('Do you run home? (1)'), nl,
+    write('Do you run back to school? (2)').
 
 describe(janitor_closet) :-
     write('You were misled and you end up in a janitor’s closet.'), nl,
     write('You look in the back and notice what appears to be a wormhole.'), nl,
-    write('Do you leave the closet? (locked)'), nl,
-    write('Do you approach the wormhole? (flying_toilets)').
+    write('Do you leave the closet? (1)'), nl,
+    write('Do you approach the wormhole? (2)').
 
 describe(locked) :-
     write('The closet door is locked.'), nl,
-    write('Do you cry in a corner? (peed_pants)'), nl,
-    write('Do you enter the wormhole? (flying_toilets)').
+    write('Do you cry in a corner? (1)'), nl,
+    write('Do you enter the wormhole? (2)').
 
 describe(flying_toilets) :-
     write('In the wormhole,'), nl,
     write('you look around and see golden toilets flying past you.'), nl,
-    write('Do you try grabbing onto one? (toilet_ride)'), nl,
-    write('Do you watch them fly by? (exit_hole)').
+    write('Do you try grabbing onto one? (1)'), nl,
+    write('Do you watch them fly by? (2)').
 
 describe(toilet_ride) :-
     write('You manage to grab onto one but notice the toilets are glued shut.'), nl,
     write('You are now traversing a wormhole on a golden toilet.'), nl,
     write('Weird stuff.'), nl,
-    write('Do you try prying it open? (exit_hole)'), nl,
-    write('Do you let go? (exit_hole)').
+    write('Do you try prying it open? (1)'), nl,
+    write('Do you let go? (2)').
 
 describe(exit_hole) :-
     write('You fall out of the hole and end up back in the building where you first started.'), nl,
     write('That was a strange experience.'), nl,
     write('It’s a wonder how your bladder still hasn’t exploded yet.'), nl,
-    write('Do you ask a passerby for directions? (find_bathroom)'), nl,
-    write('Do you run home? (friend)'), nl,
-    write('Do you go pee in a bush? (people_walking_by)').
+    write('Do you ask a passerby for directions? (1)'), nl,
+    write('Do you run home? (2)'), nl,
+    write('Do you go pee in a bush? (3)').
 
-describe(find_bathroom) :-
-    write('You can’t believe why you didn’t think of asking anyone earlier.'), nl,
-    write('You make it to the bathroom and carry on with your hackathon.'), nl,
-    write('Quest: completed.').
+/* ACTIONS */
 
-path(exit_hole, 1, find_bathroom) :-
-    
-    nl,
-    success.
+/* move to another state according to user input */
 
-
-/* Pathways */
 move(Choice) :-
-    current_state(Old_State),
-    path(Old_State, Choice, NewState),
+    current_state(OldState),
+    path(OldState, Choice, NewState),
     describe(NewState), !.
 
-move(Choice) :-
-    current_state(Old_State),
-    path(Old_State, Choice, NewState),
+/* end the game */
 
-move(Choice) :-
-    current_state(Old_State),
-    path(Old_State, Choice, NewState),
+end(peed_pants) :-
+    current_state(peed_pants),
+    goto(peed_pants, fail), !.
 
-goto(Old_State, NewState) :- 
-    current_state(Old_State),
-    describe(NewState), !.
+end(find_bathroom) :-
+    current_state(find_bathroom),
+    goto(find_bathroom, fail), !.
 
+/* jumping to another state without user input */
 
+goto(OldState, NewState) :- 
+    current_state(OldState),
+    describe(NewState), !
+
+/* PATHWAYS */
 
 goto(start, lost_in_school).
 goto(kicks_bladder, peed_pants).
 goto(need_to_go, forgot_key).
+
+goto(peed_pants, success).
+goto(find_bathroom, fail).
 
 end(peed_pants).
 end(find_bathroom).
